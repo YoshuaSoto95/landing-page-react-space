5e2397b5ca4420d0af6fa2c9abd75c67b3fb1877
Yoshua Soto <yoshuasoto95dev@gmail.com>
2025-09-15 11:18:49 -0400

    version 1.4

--- src/assets/components/Header/Header.jsx
+++ src/assets/components/Header/Header.jsx
@@ -1,22 +1,35 @@
+import { useState } from 'react'
 import './Header.css'
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <header className='header'>
             <div className="container">
                 <div className="logo">
                     <h2>Space <span>Tourism</span></h2>
                 </div>
-                <nav className="nav">
-                    <a href="" className='nav__link'>About</a>
-                    <a href="" className='nav__link'>Destinations</a>
-                    <a href="" className='nav__link'>Blog</a>
-                    <a href="" className='nav__link'>Contact</a>
+
+                {/* nav normal */}
+                <nav className={`nav ${isOpen ? 'active' : ''}`}>
+                    <a href="#about" className='nav__link'>About</a>
+                    <a href="#destinations" className='nav__link'>Destinations</a>
+                    <a href="#blog" className='nav__link'>Blog</a>
+                    <a href="#contact" className='nav__link'>Contact</a>
+                    <button className='btn-nav'>Get Started</button>
                 </nav>
+
                 <div className="cta__btn">
                     <button className='btn'>Get Started</button>
                 </div>
-                <div className="toggle__btn">
+
+                {/* botón hamburguesa */}
+                <div className={`toggle__btn ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
                     <span className="bar"></span>
                     <span className="bar"></span>
                     <span className="bar"></span>
